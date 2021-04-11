bfe90566b41216f561dc821aba71643942d7fafc
Fatih Soysal <fatihsoysal@outlook.com>
2021-04-11 11:41:51 +0300

    GiyimFiyatlandırma
    GiyimFiyatlandırma

---
+++ 11.04.Uygulamalar/GiyimFiyatlandirma/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiyimFiyatlandirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            S: 25TL
+            M: 30TL
+            L: 35TL
+            XL: 40TL
+            
+            || ÝF KONTROLÜNDE YADA ANLAMINA GELÝR.
+            && ÝF KONTROLÜNDE VE ANLAMINA GELÝR.
+            */
+
+            string beden;
+            Console.WriteLine("Lütfen beden bilgisi giriniz: (Örn: S yada M yada L yada XL)");
+            beden = Console.ReadLine();
+            beden = beden.ToUpper();
+
+            /*Console.WriteLine("Lütfen kelime yada harf yazýnýz: ");
+            string kelime = Console.ReadLine();
+            Console.WriteLine("Yazdýðýnýz kelimenin küçük harfli hali: {0}",kelime.ToLower());
+            Console.ReadLine();*/
+
+            //Burada bana küçük harf gelme ihtimali 0
+            if (beden == "S")
+                Console.WriteLine("Ürün fiyatý: 25TL");
+            else if (beden == "M")
+                Console.WriteLine("Ürün fiyatý: 30TL");
+            else if (beden == "L")
+                Console.WriteLine("Ürün fiyatý: 35TL");
+            else if (beden == "XL")
+                Console.WriteLine("Ürün fiyatý: 40TL");
+            else
+                Console.WriteLine("Ýstenilen kriterlerde bir giriþ yapmadýnýz. Lütfen S - M - L - XL giriþlerinden birini yapýnýz.");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
